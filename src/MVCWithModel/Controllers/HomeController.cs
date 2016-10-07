31ae367fd5f3d5f8aa23abc36989456945789184
olanmj <olanm@stockton.edu>
2016-10-07 13:23:09 -0400

    Home, About, Contact page formatting. Also removed references to CharacterRepo

--- src/MVCWithModel/Controllers/HomeController.cs
+++ src/MVCWithModel/Controllers/HomeController.cs
@@ -9,39 +9,43 @@ namespace MVCWithModel.Controllers
 {
     public class HomeController : Controller
     {
-        static CharacterRepo characters = new CharacterRepo();
+        // CharacterRepo replaced by a database
+      //  static CharacterRepo characters = new CharacterRepo();
         public IActionResult Index()
         {
             ViewData["Title"] = "Home";
-            return View(characters);
-        }
-
-        public IActionResult Create()
-        {
             return View();
         }
 
-        [HttpPost]
-        public IActionResult Create(Character newCharacter)
-        {
-            if (ModelState.IsValid)
-            {
-                characters.CharacterList.Add(newCharacter);
-                return RedirectToAction("Index");
-            }
-            return View(newCharacter);
-        }
+        // These are no longer used.
+        // CharacterRepo is replace by a database
+
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public IActionResult Create(Character newCharacter)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        characters.CharacterList.Add(newCharacter);
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(newCharacter);
+        //}
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "The Character Application.";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Anonymous...";
 
             return View();
         }
